ec1ceb18414987e15059d435498e89d0cd283784
celle <bobvick14@gmail.com>
2025-05-14 19:36:09 +0000

    optimization

--- find_seeds.c
+++ find_seeds.c
@@ -1,6 +1,22 @@
-// find a seed with a certain structure at the origin chunk
 #include "finders.h"
 #include <stdio.h>
+
+void getRegPos(Pos *p, uint64_t *s, int rx, int rz, StructureConfig sc)
+{
+    setSeed(s, rx*341873128712ULL + rz*132897987541ULL + *s + sc.salt);
+    p->x = ((uint64_t)rx * sc.regionSize + nextInt(s, sc.chunkRange)) << 4;
+    p->z = ((uint64_t)rz * sc.regionSize + nextInt(s, sc.chunkRange)) << 4;
+}
+
+// I think the compiler is smart enough for this to not matter but whatever
+// gotta save those like 3 cpu cycles :P
+int getFortressPosOpt(uint64_t seed, int regX, int regZ, Pos *pos)
+{
+    StructureConfig sconf = { 30084232, 27, 23, Fortress, DIM_NETHER, 0}; // fort
+
+    getRegPos(pos, &seed, regX, regZ, sconf); // idfk
+    return nextInt(&seed, 5) < 2;
+}
 
 int main()
 {
@@ -16,35 +32,29 @@ int main()
         // The structure position depends only on the region coordinates and
         // the lower 48-bits of the world seed.
         Pos p;
-        if (!getStructurePos(structType, mc, lower48, 0, 0, &p))
+        if (!getFortressPosOpt(lower48, 0, 0, &p))
             continue;
 
-        // Look for a full 64-bit seed with viable biomes, to check spawner location
-        uint64_t seed;
-        uint64_t upper16;
-        printf("seed %" PRId64 "\n", (int64_t) lower48);
+        // forts within 1000 blocks of (0,0) (calculating world spawn would take too long)
+        if (p.x > 62 || p.z > 62) {
+            continue;
+        }
 
-        // for (upper16 = 0; upper16 < 0x10000; upper16++)
-        // {
-        //     seed = lower48 | (upper16 << 48);
-        //     applySeed(&g, DIM_NETHER, seed);
-        //     if (isViableStructurePos(structType, &g, p.x, p.z, 0))
-        //     {
-        //         printf("Structure seed %" PRId64 " has a fort at chunk (%d, %d).\n",
-        //             (int64_t) lower48, p.x, p.z);
-        //         break;
-        //     }
-        // }
+        printf("structure seed %" PRId64 "\n", (int64_t) lower48);
 
-        Piece test[400];
-        int part_count = getFortressPieces(test, 400, mc, seed, p.x, p.z);
+        Piece test[400]; // store fortress pieces
+
+        // only need 48-bits of seed for structures
+        int part_count = getFortressPieces(test, 400, mc, lower48, p.x, p.z);
 
         printf("part count %d \n", part_count);
         
-        for(int n = 0; test[n].pos.z != 0; n++) {
+        int spawners = 0;
+        for(int n = 0; n <= part_count && spawners != 2; n++) {
             if (test[n].type == 5) {
                 printf("found spawner: x:%d z:%d rot:%d depth:%d\n",
                     test[n].pos.x, test[n].pos.z, test[n].rot, test[n].depth);
+                spawners++;
             }
         }
     }
